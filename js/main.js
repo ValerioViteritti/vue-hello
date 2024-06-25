bd553a358880daa0fc422f51ebea4fe77b9c7e5a
ValerioViteritti <vviteritti@gmail.com>
2024-06-25 09:46:40 +0200

    1.0.1 minor fix

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
+
+// Variabile - Funzione -Libreria Vue
 const { createApp } = Vue;
+// Prompt per chiedere nome e cognome al nuovo studente
 let dato1 = prompt('Inserisci il tuo nome');
 let dato2 = prompt('Inserisci il tuo cognnome');
 console.log(dato1, dato2);
+// Funzione strapolata da Vue
 createApp({
+    // metodo di Vue che restituisce un oggetto
     data() {
         return {
             message: 'Hello Booleaners',
@@ -16,6 +21,6 @@ createApp({
             cognome: dato2,
         }
     }
-
+// Monta l'app Vue nell'elemento con id "stampa"
 }).mount('#stampa');
 
